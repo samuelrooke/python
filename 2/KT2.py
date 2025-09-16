700e70dfa2f79e6d07d0ddfd22bd37afdb7ff7bd
Samuel <srooke50@gmail.com>
2025-09-16 13:48:49 +0300

    KT2

---
+++ 2/KT2.py
@@ -0,0 +1,10 @@
+selitys = 'Raivokkaan Asiallinen Utelias Näkemyksellinen Onnistuja'
+sanat = selitys.split()
+r = sanat [0][0] 
+a = sanat [1][0]
+u = sanat [2][0]
+n = sanat [3][0]
+o = sanat [4][0]
+nimi = f'{r}.{a}.{u}.{n}.{o}.'
+print(f"{nimi} {selitys}")
+# R.A.U.N.O.
---
+++ 2/TT4.py
@@ -0,0 +1,8 @@
+# >>> dna = 'ATATTGCTTGAAGCTGTACCGTTTTAGGGGGGGAACGCTGAAGATCTCTT'
+# >>> n_a = dna.count('A')
+# >>> n_c = dna.count('C')
+# >>> n_g = dna.count('G')
+# >>> n_t = dna.count('T')
+# >>> print(f'A: {n_a}  C: {n_c}  G: {n_g}  T: {n_t}')
+# A: 11  C: 8  G: 15  T: 16
+# >>>
